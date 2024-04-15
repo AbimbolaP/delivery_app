9e4f764b2763271970d030dd684297dbe1e4e873
AbimbolaP <abimbola.peace131@gmail.com>
2024-04-15 14:06:34 +0100

    Home component completed

---
+++ src/utils/data.js
@@ -0,0 +1,35 @@
+import I1 from "../img/i1-min.png";
+import F1 from "../img/f1.png";
+import C3 from "../img/c3-min.png";
+import Fi1 from "../img/fi1-min.png";
+    
+ export const heroData = [
+  {
+    id : 1, 
+    name : "Icecream", 
+    desc : "Double ChocBerry", 
+    price : "5.25", 
+    imageSrc: I1 
+  },
+  {
+    id : 2, 
+    name : "Strawberries", 
+    desc : "Fresh StrawBerries", 
+    price : "10.25", 
+    imageSrc: F1 
+  },
+  {
+    id : 3, 
+    name : "Chicken Kebab", 
+    desc : "Spicy Kebab Platter", 
+    price : "8.45", 
+    imageSrc: C3 
+  },
+  {
+    id : 4, 
+    name : "Fish", 
+    desc : "Sweet Sour Diner", 
+    price : "9.35", 
+    imageSrc: Fi1 
+  }
+]
